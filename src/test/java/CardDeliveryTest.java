ffd90f6cec36a403db609e07549a6a6f8e3d8472
Plotnikova Anastasiya <nast_19980728@mail.ru>
2022-03-17 23:21:06 +0300

    fix bugs

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,43 +1,28 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
-    String city;
-    String date;
-    String date2;
+
+    String planningDate = DataGenerator.generateDate(4);
+    String planningDate2 = DataGenerator.generateDate(8);
     String name;
+    String city;
     String tel;
-
-    String planningDate = generateDate(4);
-
-    String generateDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-
-    private Faker faker;
 
     @BeforeEach
     void setUp() {
-        faker = new Faker(new Locale("ru"));
-        this.name = faker.name().fullName();
-        this.city = faker.address().city();
-        this.tel = faker.phoneNumber().phoneNumber();
-        this.date = generateDate(4);
-        this.date2 = generateDate(8);
+        name = DataGenerator.generateName();
+        city = DataGenerator.generateCity();
+        tel = DataGenerator.generatePhone();
     }
 
     @Test
@@ -47,16 +32,17 @@ public class CardDeliveryTest {
         $("[data-test-id=city] input").setValue(city);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $("[data-test-id=date] input").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue(name);
         $("[data-test-id=phone] input").setValue(tel);
         $(By.className("checkbox__box")).click();
         $(".button__text").click();
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15));
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $("[data-test-id=date] input").sendKeys(Keys.DELETE);
-        $("[data-test-id=date] input").setValue(date2);
+        $("[data-test-id=date] input").setValue(planningDate2);
         $(".button__text").click();
         $("[data-test-id=replan-notification] button").click();
-        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + date2), Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate2), Duration.ofSeconds(15));
     }
 }
--- src/test/java/DataGenerator.java
+++ src/test/java/DataGenerator.java
@@ -1,9 +1,42 @@
-import lombok.Data;
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class DataGenerator {
-    public static class Registration {
-        private Registration() {}
+    String city;
+    String name;
+    String phone;
 
+    DataGenerator(String city, String name, String phone) {
+        this.city = city;
+        this.name = name;
+        this.phone = phone;
     }
 
+    public static String generateCity() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.address().city();
+    }
+
+    public static String generateName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+    }
 }
